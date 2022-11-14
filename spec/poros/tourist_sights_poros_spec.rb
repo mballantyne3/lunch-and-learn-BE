fdb56acfe52cd93de72620c49a5be56470a66f1d
Mary Ballantyne <mballantyne777@gmail.com>
2022-11-14 12:04:00 -0700

    passing Tourist Sight Poro spec

--- spec/poros/tourist_sights_poros_spec.rb
+++ spec/poros/tourist_sights_poros_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'tourist sights poro' do
       {
         id: nil,
         type: 'tourist_sight',
-        attributes: {
+        properties: {
           name: "Maison de Chateaubriand",
           address: "87 Rue de Chateaubriand, 92290 Châtenay-Malabry, France",
           place_id: "515d1f5d62721d024059cf1b831ae7624840f00102f901de515805000000009203174d6169736f6e2064652043686174656175627269616e64"
@@ -16,6 +16,6 @@ RSpec.describe 'tourist sights poro' do
     sight_results = TouristSightPoro.new(data)
 
     expect(sight_results).to be_a(TouristSightPoro)
-    expect(sight_results[:attributes][:name]).to eq("Maison de Chateaubriand")
+    expect(sight_results.attributes[:name]).to eq("Maison de Chateaubriand")
   end
 end
