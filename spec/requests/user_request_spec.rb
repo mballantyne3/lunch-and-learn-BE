affbded40b158f8b945be981903aaafd758da584
Mary Ballantyne <mballantyne777@gmail.com>
2022-11-15 10:04:28 -0700

    fixed spacing and removed pp response

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -14,11 +14,11 @@ RSpec.describe 'user' do
 
       parsed_response = JSON.parse(response.body, symbolize_names: true)
 
-      pp parsed_response
       expect(parsed_response).to be_a Hash
       expect(parsed_response).to have_key :data
       expect(parsed_response[:data]).to have_key(:id)
       expect(parsed_response[:data]).to have_key(:type)
+
       expect(parsed_response[:data][:attributes]).to have_key(:name)
       expect(parsed_response[:data][:attributes][:name]).to be_a String
 
