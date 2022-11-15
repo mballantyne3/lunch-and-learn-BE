0248c8ca09a828b9e03d7515d0ffeba98c67afb2
Mary Ballantyne <mballantyne777@gmail.com>
2022-11-15 11:25:36 -0700

    create favorites table in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_14_232742) do
+ActiveRecord::Schema.define(version: 2022_11_15_181735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "recipes", force: :cascade do |t|
-    t.string "title"
-    t.string "url"
+  create_table "favorites", force: :cascade do |t|
     t.string "country"
-    t.string "image"
+    t.string "recipe_link"
+    t.string "recipe_title"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,5 +32,4 @@ ActiveRecord::Schema.define(version: 2022_11_14_232742) do
     t.datetime "updated_at", null: false
     t.string "api_key"
   end
-
 end
